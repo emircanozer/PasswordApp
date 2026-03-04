11274c2f13b2c12d8843dce49640a6f0e9a94654
emircan özer <m.emircanozer@outlook.com>
2026-03-04 16:43:18 +0300

    feat : create new password project

---
+++ Password/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Emircan Özer on 4.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
